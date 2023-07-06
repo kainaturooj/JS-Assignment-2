d9a38a4c7d693e42f05ed05c0e69c3313aea70dc
kainaturooj <128933130+kainaturooj@users.noreply.github.com>
2023-07-06 18:32:05 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+////Q...Declare a variable called username.
+
+///************coding************* */
+// var userName;
+//let userName;
+
+
+
+///Q..2. Declare a variable called myName & assign to it a string
+// that represents your Full Name.
+
+
+///************coding************* */
+// let myName = "Kaneez Fatima";
+
+
+
+///Q..3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+
+///************coding************* */
+// let messege = "Salam to every Muslim ";
+// alert(messege);
+
+
+
+
+///Q..Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+
+///************coding************* */
+// let studentName = "Kaneez Fatima";
+// let studentAge = 23;
+// let studentCourse = "Certified for Web And App Development ";
+// alert(studentName );
+// alert(studentAge);
+// alert(studentCourse);
+
+
+
+
+///Q..Write a script to display the following alert using one JS
+// variable:
+
+
+///************coding************* */
+// let message = "pizza \n pizz \n piz \n pi \n p";
+// alert(message);
+
+
+
+
+
+///Q...Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+
+
+///************coding************* */
+// let email ="kaneezfatima@gmail.com";
+// alert("My email " + email); 
+
+
+
+
+///Q...Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+
+
+///************coding************* */
+// let book = "A Smarter Way to Learn JavaScript";
+// alert("I am trying to learn from the book " + book);
+
+
+///Q..Write a script to display this in browser through JS
+// document.write("I can write the HTML content through JS");
+
+
